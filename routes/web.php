40be93f3bdebf319697dad161f80e87acb0c7343
MostafaBen <bmostapha0123@gmail.com>
2024-01-26 23:04:38 +0100

    optimize code from mostafa

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,4 @@ Route::get('/', function () {
 });
 Route::get('/livres', [LivreController::class, 'index'])->name('livres.index');
 
-Route::get('/livres/create', [LivreController::class, 'create'])->name('livres.create');
-Route::post('/livres', [LivreController::class, 'store'])->name('livres.store');
-Route::get('/livres/{livre}', [LivreController::class, 'show'])->name('livres.show');
-Route::get('/livres/{livre}/edit', [LivreController::class, 'edit'])->name('livres.edit');
-Route::put('/livres/{livre}', [LivreController::class, 'update'])->name('livres.update');
-Route::delete('/livres/{livre}', [LivreController::class, 'destroy'])->name('livres.destroy');
+Route::resource('/livres', LivreController::class);
